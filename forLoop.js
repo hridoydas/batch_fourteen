2694acd41e671132400bc0ddb586efcd35bbb47c
hridoy <hridoydasewu@gmail.com>
2025-12-09 21:45:22 +0600

    move forloop into class folder

--- forLoop.js
+++
@@ -1,7 +0,0 @@
-const arrayData = ["data_1", 2, "data_3", "hello", "batch_14"];
-
-for (let i = 0; i < arrayData.length; i++) {
-  if (arrayData[i] === "hello") {
-    console.log("Get hello");
-  }
-}
